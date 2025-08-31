57981a2cf07e15fb8859c27f868603a50d5359a7
juhwan1011 <145476715+Juhwan0611@users.noreply.github.com>
2025-09-01 00:58:26 +0900

    [Silver V] Title: 단어 정렬, Time: 80 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -3,11 +3,14 @@ input = sys.stdin.readline
 
 n = int(input())
 
-arr = [input().strip() for _ in range(n)]
+arr = []
+for _ in range(n):
+    s = input().strip() # 개행 제
+    arr.append(s)
+
 arr = list(set(arr))
 
 arr.sort(key = lambda x: (len(x), x))
 
 for x in arr:
     print(x)
-
