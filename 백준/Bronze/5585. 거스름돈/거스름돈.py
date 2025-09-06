428ce745c1a38fcb721eaf66b7304338b96fa3db
juhwan1011 <145476715+Juhwan0611@users.noreply.github.com>
2025-09-07 03:31:35 +0900

    [Bronze II] Title: 거스름돈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+money = [500, 100, 50, 10, 5, 1]
+
+num = 1000 - n
+cnt = 0
+while num:
+    for i in money:
+        cnt += num // i
+        num = num % i
+
+print(cnt)
