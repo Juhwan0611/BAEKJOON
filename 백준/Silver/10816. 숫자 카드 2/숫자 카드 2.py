4cc2e4681b580a09031f1cdcc474a5cfc25c3222
juhwan1011 <145476715+Juhwan0611@users.noreply.github.com>
2025-09-16 13:57:00 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 724 ms, Memory: 111872 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+from collections import defaultdict
+
+n = int(input())
+d = dict()
+for x in map(int,input().split()):
+    if x in d:
+        d[x] += 1
+    else:
+        d[x] = 1
+
+m = int(input())
+for x in map(int,input().split()):
+    if x in d:
+        print(d[x], end = ' ')
+    else:
+        print(0, end = ' ')
+
