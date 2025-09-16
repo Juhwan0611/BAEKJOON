874d2113bf904f808ff20980d84100b728bed477
juhwan1011 <145476715+Juhwan0611@users.noreply.github.com>
2025-09-16 13:00:48 +0900

    [Silver IV] Title: solved.ac, Time: 132 ms, Memory: 37164 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.py
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -1,24 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-from collections import deque
-
 n = int(input())
-
-if n <= 0:
+if n == 0:
     print(0)
-    
 else:
-    q = [int(input()) for _ in range(n)]
-    q = deque(sorted(q))
+    num = [int(input()) for _ in range(n)]
 
-    cnt = 0 # 없애야 할 사람의
+    num.sort()
     cnt = int((n * 0.15) + 0.5)
 
-    for _ in range(cnt):
-        q.popleft()
-    for _ in range(cnt):
-        q.pop()
-
-    avg = int((sum(q)) / len(q) + 0.5)
+    num = num[cnt:n - cnt]
+    avg = int(sum(num) / len(num) + 0.5)
     print(avg)
