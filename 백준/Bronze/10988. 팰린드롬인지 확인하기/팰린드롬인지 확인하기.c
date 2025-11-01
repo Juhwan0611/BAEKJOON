856ea5a6952a57ee6ab2c882065a57bed7414420
juhwan1011 <145476715+Juhwan0611@users.noreply.github.com>
2025-11-01 16:31:34 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char s[101];
+	scanf("%s", s);
+	
+	for (int i = 0; i < strlen(s) / 2; i++) {
+		if (s[i] != s[strlen(s) - i - 1]) {
+			printf("0");
+			return 0;
+		}
+	}
+	
+	printf("1");
+
+	return 0;
+}
