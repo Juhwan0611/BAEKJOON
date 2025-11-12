9d5be99ab8add085da579278f674f30ab9e9379f
juhwan1011 <145476715+Juhwan0611@users.noreply.github.com>
2025-11-12 19:53:13 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int f[21];
+    f[0] = 0;
+    f[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        f[i] = f[i - 1] + f[i - 2];
+    }
+
+    printf("%d", f[n]);
+
+    return 0;
+}
