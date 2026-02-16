90d9c57ba79ce1383787fb067e918a9617fa4afa
juhwan1011 <145476715+Juhwan0611@users.noreply.github.com>
2026-02-17 01:24:55 +0900

    [Silver II] Title: 에디터, Time: 32 ms, Memory: 2944 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string s = "";
+	cin >> s;
+
+	stack<char> left, right;
+	for (int i : s) left.push(i);
+
+	int m;
+	cin >> m; //명령어 개수
+
+	while (m--) {
+		char cmd;
+		cin >> cmd;
+
+		if (cmd == 'L') {
+			if (!left.empty()) {
+				right.push(left.top());
+				left.pop();
+			}
+		}
+		else if (cmd == 'D') {
+			if (!right.empty()) {
+				left.push(right.top());
+				right.pop();
+			}
+		}
+		else if (cmd == 'B') {
+			if (!left.empty()) {
+				left.pop();
+			}
+		}
+		else if (cmd == 'P') {
+			char c;
+			cin >> c;
+			left.push(c);
+		}
+	}
+
+	//왼쪽에 있는 것들 오른쪽으로 옮겨서 문자열 완성하기
+	while (!left.empty()) { //커서가 맨 앞에 있으면 이미 완성된거임
+		right.push(left.top());
+		left.pop();
+	}
+
+	while (!right.empty()) {
+		cout << right.top();
+		right.pop();
+	}
+
+	return 0;
+}
