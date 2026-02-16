65d90d2e574ccd0e767fd6c945a4ed6707d0d7a7
juhwan1011 <145476715+Juhwan0611@users.noreply.github.com>
2026-02-17 01:39:11 +0900

    [Bronze II] Title: 분산처리, Time: 772 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	while (n--) {
+		int a, b;
+		cin >> a >> b;
+
+		int res = 1;
+		for (int i = 0; i < b; i++) {
+			res = (res * a) % 10;
+		}
+
+		if (res == 0) {
+			cout << 10 << "\n";
+		}
+		else {
+			cout << res << "\n";
+		}
+	}
+
+	return 0;
+}
