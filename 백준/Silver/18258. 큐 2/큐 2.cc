61089213790dcecdc68f0925042abdf7aa5543c4
juhwan1011 <145476715+Juhwan0611@users.noreply.github.com>
2026-03-03 09:27:18 +0900

    [Silver IV] Title: 큐 2, Time: 320 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	queue<int> v;
+
+	while (n--) {
+		string s;
+		cin >> s;
+
+		if (s == "push") {
+			int num;
+			cin >> num;
+			v.push(num);
+		}
+
+		else if (s == "pop") {
+			if (v.empty()) {
+				cout << -1 << "\n";
+			}
+			else {
+				cout << v.front() << "\n";
+				v.pop();
+			}
+		}
+
+		else if (s == "size") {
+			cout << v.size() << "\n";
+		}
+
+		else if (s == "empty") {
+			if (v.empty()) {
+				cout << 1 << "\n";
+			}
+			else {
+				cout << 0 << "\n";
+			}
+		}
+
+		else if (s == "front") {
+			if (v.empty()) {
+				cout << -1 << "\n";
+			}
+			else {
+				cout << v.front() << "\n";
+			}
+		}
+
+		else if (s == "back") {
+			if (v.empty()) {
+				cout << -1 << "\n";
+			}
+			else {
+				cout << v.back() << "\n";
+			}
+		}
+	}
+
+	return 0;
+}
