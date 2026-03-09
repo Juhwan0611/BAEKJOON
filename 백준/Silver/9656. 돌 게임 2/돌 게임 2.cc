757723832a2e5c752710b7745bfa76fe3ae2c987
juhwan1011 <145476715+Juhwan0611@users.noreply.github.com>
2026-03-09 16:22:47 +0900

    [Silver V] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	if (n % 2 == 0) {
+		cout << "SK" << "\n";
+	}
+
+	else {
+		cout << "CY" << "\n";
+	}
+
+	return 0;
+}
